ce5d86f9d93544396b02de7f5c11dfbc87b2b3c5
Federico.Murru <djfedemurru@icloud.com>
2024-09-19 18:15:03 +0100

    .

--- app/components/MiddleSection.js
+++ app/components/MiddleSection.js
@@ -15,37 +15,43 @@ export default function HeroSection() {
 					system to treat not just your pain, but to improve your quality of
 					life for the long term.
 				</p>
-				<a
-					href="/services"
-					className="inline-flex items-center text-blue-500 font-semibold hover:underline"
-				>
-					Learn More
-					<svg
-						className="ml-2 w-4 h-4"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth={2}
-							d="M9 5l7 7-7 7"
-						/>
-					</svg>
-				</a>
 			</div>
 
-			{/* Right Section - Image */}
-			<div className="md:w-1/2 w-full px-6 mt-8 md:mt-0 flex justify-center">
-				<Image
-					src="/service2.jpg" // Update this path with your actual image
-					alt="Doctor and Patient"
-					width={500}
-					height={400}
-					className="rounded-md shadow-lg"
-				/>
+			{/* Right Section - Image and Button */}
+			<div className="md:w-1/2 w-full px-6 mt-8 md:mt-0 flex flex-col items-center justify-center">
+				<div className="flex justify-center">
+					<Image
+						src="/service2.jpg" // Update this path with your actual image
+						alt="Doctor and Patient"
+						width={500}
+						height={300}
+						className="rounded-md shadow-lg"
+					/>
+				</div>
+
+				{/* Learn More button - At bottom of image on small screens */}
+				<div className="w-full mt-4 md:mt-10">
+					<a
+						href="/services"
+						className="inline-flex items-center justify-center text-blue-500 font-semibold hover:underline w-full md:w-auto mt-4 md:mt-0"
+					>
+						Learn More
+						<svg
+							className="ml-2 w-4 h-4"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M9 5l7 7-7 7"
+							/>
+						</svg>
+					</a>
+				</div>
 			</div>
 		</section>
 	);
