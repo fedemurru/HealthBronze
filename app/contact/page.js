4292982221eaac73d4d5973194dab4c317cd42ea
Federico.Murru <djfedemurru@icloud.com>
2024-09-19 18:03:17 +0100

    .env

--- app/contact/page.js
+++ app/contact/page.js
@@ -7,32 +7,60 @@ import Alert from "../components/Alert.js";
 
 export default function Contact() {
 	const formRef = useRef();
-
 	const { alert, showAlert, hideAlert } = useAlert();
 	const [loading, setLoading] = useState(false);
-
+	const [errors, setErrors] = useState({});
 	const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+	const validateEmail = (email) => {
+		const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return re.test(email);
+	};
+
+	const validateForm = () => {
+		let formErrors = {};
+		let valid = true;
+
+		if (!form.name || form.name.length < 3) {
+			formErrors.name = "Name must be at least 3 characters.";
+			valid = false;
+		}
+
+		if (!form.email || !validateEmail(form.email) || form.email.length < 6) {
+			formErrors.email = "Please enter a valid email address.";
+			valid = false;
+		}
+
+		if (!form.message || form.message.length < 10) {
+			formErrors.message = "Message must be at least 10 characters.";
+			valid = false;
+		}
+
+		setErrors(formErrors);
+		return valid;
+	};
 
 	const handleChange = ({ target: { name, value } }) => {
 		setForm({ ...form, [name]: value });
+		setErrors({ ...errors, [name]: "" }); // Reset the error message for the specific field
 	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		setLoading(true);
 
-		if (!form.name || !form.email || !form.message) {
+		if (!validateForm()) {
 			setLoading(false);
-			showAlert({
-				show: true,
-				text: "Please fill in all the fields",
-				type: "danger",
-			});
+			// showAlert({
+			// 	show: true,
+			// 	text: "Please fix the highlighted errors.",
+			// 	type: "danger",
+			// });
 			return;
 		}
 
 		try {
-			// Invio dell'email usando EmailJS
+			// Send the email using EmailJS
 			await emailjs.send(
 				process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, // Service ID from .env
 				process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, // Template ID from .env
@@ -45,10 +73,8 @@ export default function Contact() {
 				},
 				process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY // Public Key from .env
 			);
-			//service_lg30a2k
-			// template_908by6p
 
-			// Se l'invio ha successo
+			// If email sending is successful
 			setLoading(false);
 			showAlert({
 				show: true,
@@ -58,14 +84,10 @@ export default function Contact() {
 
 			setTimeout(() => {
 				hideAlert(false);
-				setForm({
-					name: "",
-					email: "",
-					message: "",
-				});
+				setForm({ name: "", email: "", message: "" });
 			}, 3000);
 		} catch (error) {
-			// Se l'invio fallisce
+			// If email sending fails
 			setLoading(false);
 			console.error("Email send error: ", error);
 
@@ -97,8 +119,11 @@ export default function Contact() {
 						onChange={handleChange}
 						required
 						placeholder="ex., John Doe"
-						className="w-full px-4 py-2 border rounded-lg"
+						className={`w-full px-4 py-2 border rounded-lg ${
+							errors.name ? "border-red-500" : ""
+						}`}
 					/>
+					{errors.name && <p className="text-red-500">{errors.name}</p>}
 				</div>
 				<div className="mb-4">
 					<label className="block text-gray-700">Email</label>
@@ -109,8 +134,11 @@ export default function Contact() {
 						onChange={handleChange}
 						required
 						placeholder="ex., johndoe@gmail.com"
-						className="w-full px-4 py-2 border rounded-lg"
+						className={`w-full px-4 py-2 border rounded-lg ${
+							errors.email ? "border-red-500" : ""
+						}`}
 					/>
+					{errors.email && <p className="text-red-500">{errors.email}</p>}
 				</div>
 				<div className="mb-4">
 					<label className="block text-gray-700">Message</label>
@@ -121,8 +149,11 @@ export default function Contact() {
 						required
 						rows={5}
 						placeholder="Share your inquiries..."
-						className="w-full px-4 py-2 border rounded-lg"
+						className={`w-full px-4 py-2 border rounded-lg ${
+							errors.message ? "border-red-500" : ""
+						}`}
 					></textarea>
+					{errors.message && <p className="text-red-500">{errors.message}</p>}
 				</div>
 				<button
 					type="submit"
